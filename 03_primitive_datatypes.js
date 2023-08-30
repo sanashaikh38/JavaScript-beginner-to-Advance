a32255296145e20cba76dd2392c24f6d02bbc90d
sanashaikh38 <123015784+sanashaikh38@users.noreply.github.com>
2023-08-31 00:45:11 +0530

    Add files via upload

---
+++ 03_primitive_datatypes.js
@@ -0,0 +1,21 @@
+//nn bb ss u
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt('567')
+let e='hi'
+let f =Symbol('I am nice symbol')
+let g=undefined
+console.log(a,b,c,d,e,f,g,)
+console.log(typeof c)
+
+
+// non primitive data type object in js
+const item =
+  {
+    'harry':true,'sana':false,'rita':90
+,'rohan':undefined  }
+
+
+console.log(item["sana"])
+
