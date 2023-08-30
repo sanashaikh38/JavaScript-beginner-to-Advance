d5ee005c0697472cfc6ff42d54ade2a3a5eaaa29
sanashaikh38 <123015784+sanashaikh38@users.noreply.github.com>
2023-08-31 01:06:22 +0530

    Add files via upload

---
+++ 04_chapter_1_practice_set.js
@@ -0,0 +1,38 @@
+// Q.1 create a variable of type string and try to add a number to it
+let a="sana"
+let b=5
+console.log(a+b)
+
+// Q.2 use typeof oprator to find the type of string in last question
+console.log(typeof(a+b))
+
+// Q.3 create a const object in javascript can you change it to hold number later? ans=NO 
+
+const item={
+  name:'sana',
+  section:1,
+  isprincipal:false
+  
+}
+// name=45
+console.log(item)
+
+// Q.4 try to add new key to const object in problem 3 were you able to do it? ans =yes
+
+item['friend']='shubham'
+console.log(item)
+
+// Q.5 write a js program to word meaning dictionary of 4 words
+
+const dic={
+  Ambigue:'An ambiguous expression or statement.',
+  Athleisure:'Comfortable and casual footwear & clothing designed for exercise and rigorous activity.'
+  ,
+  Broigus:'Angry or irritated.',
+  
+  'By-Catch':'	A catch of fish that cannot be put to commercial use.'
+  
+}
+
+console.log(dic['Ambigue'])
+
