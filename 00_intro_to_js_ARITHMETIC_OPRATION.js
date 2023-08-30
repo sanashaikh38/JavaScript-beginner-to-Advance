c46d149f33bcdbd6a8758ac0be0d85318faafcb3
sanashaikh38 <123015784+sanashaikh38@users.noreply.github.com>
2023-08-30 22:38:59 +0530

    Add files via upload

---
+++ 00_intro_to_js_ARITHMETIC_OPRATION.js
@@ -0,0 +1,11 @@
+console.log("Let's get Started")
+function  num_arithmetic_oprations(a,b)
+{ c=console.log('the addition of numbers is:' ,a+b);
+ d=console.log('the substraction of numbers is :', a-b);
+ e=console.log('the multiplication of numbers is :' ,a*b);
+ f=console.log('the division of numbers is: ', a/b);
+ return d,e,f,c;
+}
+
+g=num_arithmetic_oprations(9,4)
+console.log(g)
