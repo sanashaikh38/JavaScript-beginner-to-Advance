d354022c11e56a04aa95123d3925914fe6b818d3
Sana Shaikh <123015784+sanashaikh38@users.noreply.github.com>
2023-09-04 03:28:23 +0530

    Add files via upload

---
+++ 05_Operator_and_Expression.js
@@ -0,0 +1,47 @@
+console.log("Oprator and Expressions in Js")
+let a=45;
+let b =4;
+
+//Arithmetic oprators
+console.log("a+ b = ",a+b)
+console.log("a- b = ",a-b)
+console.log("a* b = ",a*b)
+console.log("a /b = ",a/b)
+console.log("a ** b = ",a**b)
+console.log("a % b = ", a%b)
+
+// Increment and Decrement Oprators
+console.log("a++  =",a++)
+console.log("++a  =",++a)
+console.log("a = ",a)
+console.log("a-- = ",a--)
+console.log("--a = ",--a)
+console.log("a = ",a)
+
+//Assingment oprators
+let assi=1;
+console.log('assi= ',assi)
+assi+=5 //same as assi=assin+5
+console.log("Now assi has value= ",assi)
+
+//Comparision oprator
+
+let num1=3;
+let num2=7;
+console.log("num1",num1)
+console.log("num2",num2)
+console.log("num1==num2 is ",num1==num2)
+console.log("num1!=num2 is ",num1!=num2)
+console.log("num1===num2 is ",num1===num2)
+console.log("num1!==num2 is",num1!==num2)
+console.log('num1 > num2 is ', num1>num2)
+console.log('num1<num2 is ',num1<num2)
+
+// logical operator 
+let x=5;
+let y=6;
+console.log(x<y       && x==5)
+console.log(x>y|| x!=5)
+console.log(!false)
+console.log(!true)
+
